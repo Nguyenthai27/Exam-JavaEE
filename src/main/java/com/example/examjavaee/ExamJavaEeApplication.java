2141999eb940048d31f2742bda2eade342a5d8cd
Nguyenthai27 <ptejthaii@gmail.com>
2023-12-09 19:21:30 +0700

    init commit

---
+++ src/main/java/com/example/examjavaee/ExamJavaEeApplication.java
@@ -0,0 +1,13 @@
+package com.example.examjavaee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamJavaEeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamJavaEeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examjavaee/ExamJavaEeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examjavaee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamJavaEeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
